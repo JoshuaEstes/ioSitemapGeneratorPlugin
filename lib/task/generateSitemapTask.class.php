fc553aa455edef7c3ce3a1f791c5a751a0474af7
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2011-12-20 07:48:16 -0600

    code clean up, added some comments, updated readme file

--- lib/task/generateSitemapTask.class.php
+++ lib/task/generateSitemapTask.class.php
@@ -1,26 +1,33 @@
 <?php
 
+/**
+ * 
+ *
+ * @author
+ * @copyright
+ * @package    ioSitemapGeneratorPlugin
+ * @subpackage
+ * @version
+ *
+ */
 class generateSitemapTask extends sfBaseTask
 {
 
+  /**
+   *
+   */
   protected function configure()
   {
-    // // add your own arguments here
-    // $this->addArguments(array(
-    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
-    // ));
-
     $this->addOptions(array(
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
       new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
       new sfCommandOption('sf_host', null, sfCommandOption::PARAMETER_REQUIRED, 'The host, example: http://www.example.com'),
-      // add your own options here
     ));
 
     $this->namespace = 'generate';
     $this->name = 'sitemap';
-    $this->briefDescription = '';
+    $this->briefDescription = 'Generates a sitemap';
     $this->detailedDescription = <<<EOF
 The [generate:sitemap|INFO] task does things.
 Call it with:
@@ -29,35 +36,41 @@ Call it with:
 EOF;
   }
 
+  /**
+   *
+   * @param array $arguments
+   * @param array $options
+   */
   protected function execute($arguments = array(), $options = array())
   {
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
+    // If the user did not pass the option sf_host then grab it from the config
     if (null === $options['sf_host'])
     {
       $options['sf_host'] = sfConfig::get('app_ioSitemapGenerator_sf_host');
     }
 
-    $models = sfConfig::get('app_ioSitemapGenerator_models');
-    $routes = $this->getRouting()->getRoutes();
-
+    // Create the xml sitemap
     $doc = new DOMDocument('1.0', 'UTF-8');
     $urlset = $doc->createElement('urlset');
     $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
     $doc->appendChild($urlset);
 
+    // Grab a list of models we want to generate a sitemap for
+    $models = sfConfig::get('app_ioSitemapGenerator_models');
+
+    /**
+     * $urlNB is only used to count the number of links that we place in the
+     * sitemap.xml file
+     */
     $urlNB = 0;
     foreach ($models as $model)
     {
-      if (!isset($routes[$model['route']]))
-      {
-        throw new sfException('Cannot find route');
-      }
       /* @var $modelTable Doctrine_Table */
       $modelTable = Doctrine_Core::getTable($model['model']);
-      /* @var $route sfDoctrineRoute */
-      $route = $routes['category_show'];
+      /* @var $collection Doctrine_Collection */
       $collection = $modelTable->findAll();
       foreach ($collection as $record)
       {
@@ -66,22 +79,23 @@ EOF;
         $uri = $options['sf_host'] . $this->getRouting()->generate($model['route'], $record);
         $this->logSection('uri+', $uri);
         $url->appendChild($doc->createElement('loc', $uri));
+
         if (isset($model['changefreq']))
         {
           $this->logSection('changefreq+', $model['changefreq'], null, 'COMMENT');
-          $url->appendChild($doc->createElement('changefreq',$model['changefreq']));
+          $url->appendChild($doc->createElement('changefreq', $model['changefreq']));
         }
 
         if (isset($model['priority']))
         {
           $this->logSection('priority+', $model['priority'], null, 'COMMENT');
-          $url->appendChild($doc->createElement('priority',$model['priority']));
+          $url->appendChild($doc->createElement('priority', $model['priority']));
         }
 
-        if ((!isset($model['include_lastmod']) || !$model['include_lastmod']) &&  $modelTable->hasColumn('updated_at'))
+        if ((!isset($model['include_lastmod']) || !$model['include_lastmod']) && $modelTable->hasColumn('updated_at'))
         {
           $this->logSection('lastmod+', $record->updated_at, null, 'COMMENT');
-          $url->appendChild($doc->createElement('lastmod',$record->updated_at));
+          $url->appendChild($doc->createElement('lastmod', $record->updated_at));
         }
 
         $urlset->appendChild($url);
@@ -89,6 +103,7 @@ EOF;
       }
     }
 
+    // Grab the urls
     $urls = sfConfig::get('app_ioSitemapGenerator_urls');
     foreach ($urls as $record)
     {
@@ -100,15 +115,15 @@ EOF;
       if (isset($record['changefreq']))
       {
         $this->logSection('changefreq+', $record['changefreq'], null, 'COMMENT');
-        $url->appendChild($doc->createElement('changefreq',$record['changefreq']));
+        $url->appendChild($doc->createElement('changefreq', $record['changefreq']));
       }
 
       if (isset($record['priority']))
       {
         $this->logSection('priority+', $record['priority'], null, 'COMMENT');
-        $url->appendChild($doc->createElement('priority',$record['priority']));
+        $url->appendChild($doc->createElement('priority', $record['priority']));
       }
-      
+
       $urlset->appendChild($url);
       $urlNB++;
     }
@@ -118,7 +133,7 @@ EOF;
     $filename = sfConfig::get('sf_web_dir') . '/sitemap.xml';
     $this->getFilesystem()->touch($filename);
     file_put_contents($filename, $doc->saveXML());
-    $this->logBlock(sprintf('Total URLs: %d',$urlNB), 'COMMENT_LARGE');
+    $this->logBlock(sprintf('Total URLs: %d', $urlNB), 'COMMENT_LARGE');
   }
 
 }
